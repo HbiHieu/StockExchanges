8c62ef2756b18b6576cdbe10a46f72c22ff8b069
minhngth2409 <minhngth2409@gmail.com>
2023-02-12 16:16:43 +0700

    create check sign middleware

---
+++ server/src/interfaces/middleware.ts
@@ -0,0 +1,5 @@
+
+
+export interface IResponse extends Response{
+    status: number
+}
---
+++ server/src/middlewares/sign-verify.middleware.ts
@@ -0,0 +1,23 @@
+import {HttpStatus, Injectable} from "@nestjs/common";
+import {NextFunction, query} from "express";
+import {MRequest} from "../types/middleware";
+import {ExceptionResponse} from "../utils/utils.exception";
+import * as crypto from "crypto";
+
+
+@Injectable()
+export class SignVerifyMiddleware {
+    use(req: MRequest, res: Response, next: NextFunction) {
+        const sign = req.headers.sign;
+        const data = JSON.stringify({query: req.query, params: req.params})
+        if (!sign) throw new ExceptionResponse(HttpStatus.UNAUTHORIZED, 'Sign is not valid!');
+
+        //generate serverSign depend on params and query to compare
+        const serverSign = crypto.createHmac('sha256', process.env.SECRET_SIGN_KEY)
+            .update(data)
+            .digest('hex')
+
+        if (sign != serverSign) throw new ExceptionResponse(HttpStatus.UNAUTHORIZED, 'Sign is not valid!');
+        next()
+    }
+}
